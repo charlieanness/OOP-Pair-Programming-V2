0ae7ad0b0444a39fff4e2cc9df17da7aea48f494
charlieanness <crta201@exeter.ac.uk>
2026-03-02 16:47:41 +0000

    ran more tests, think we're done now

--- src/main/java/cityrescue/temporaryTesting.java
+++ src/main/java/cityrescue/temporaryTesting.java
@@ -3,112 +3,126 @@ package cityrescue;
 import cityrescue.enums.*;
 
 /**
- * This class is here solely for separate individual tests that I wanted to run
- * during debugging, etc
+ * This class is here solely for separate individual tests and edge cases 
+ * that I wanted to run during debugging, etc
+ * Tests have been commented out as they are not needed,
+ * but they prove as evidence to show SOME of my tests
+ * throughout development.
+ * Note that this code will error if it is all uncommented. This is because
+ * it is simply a collection of tests that dont necessarily all relate to eachother.
  */
 public class temporaryTesting {
 
     public static void main(String[] args) throws Exception
     {
-        CityRescue cr;
-        cr = new CityRescueImpl();
-        cr.initialise(5, 5);
 
-        int[] sz = cr.getGridSize();
+        // CityRescue cr;
+        // cr = new CityRescueImpl();
+        // cr.initialise(5, 5);
 
-        cr.addStation("A", 2, 3);
-        cr.addUnit(1, UnitType.AMBULANCE);
-        cr.addUnit(1, UnitType.POLICE_CAR);
-        cr.reportIncident(IncidentType.MEDICAL, 3, 3, 3);
+        //int[] sz = cr.getGridSize();
 
-        System.out.println(sz);
-        System.out.println(cr.getStatus());
+        // cr.addStation("A", 2, 3);
+        // cr.addUnit(1, UnitType.AMBULANCE);
+        // cr.addUnit(1, UnitType.POLICE_CAR);
+        // cr.reportIncident(IncidentType.MEDICAL, 3, 3, 3);
 
-        cr.dispatch();
-        cr.tick();
-        System.out.println(cr.getStatus());
+        // System.out.println(sz);
+        // System.out.println(cr.getStatus());
 
-        cr.tick();
-        System.out.println(cr.getStatus());
+        // cr.dispatch();
+        // cr.tick();
+        // System.out.println(cr.getStatus());
 
-        cr.tick();
-        System.out.println(cr.getStatus());
+        // cr.tick();
+        // System.out.println(cr.getStatus());
 
-        cr.decommissionUnit(1);
-        System.out.println(cr.getStatus());
+        // cr.tick();
+        // System.out.println(cr.getStatus());
 
-        cr.reportIncident(IncidentType.CRIME, 1, 2, 2);
-        cr.dispatch();
-        cr.tick();
-        System.out.println(cr.getStatus());
+        // cr.decommissionUnit(1);
+        // System.out.println(cr.getStatus());
 
-        cr.tick();
-        System.out.println(cr.getStatus());
+        // cr.reportIncident(IncidentType.CRIME, 1, 2, 2);
+        // cr.dispatch();
+        // cr.tick();
+        // System.out.println(cr.getStatus());
 
-        cr.tick();
-        System.out.println(cr.getStatus());
+        // cr.tick();
+        // System.out.println(cr.getStatus());
 
-        cr.tick();
-        System.out.println(cr.getStatus());
+        // cr.tick();
+        // System.out.println(cr.getStatus());
 
-        cr.reportIncident(IncidentType.CRIME, 1, 3, 3);
+        // cr.tick();
+        // System.out.println(cr.getStatus());
 
-        cr.dispatch();
-        cr.tick();
-        System.out.println(cr.getStatus());
+        // cr.reportIncident(IncidentType.CRIME, 1, 3, 3);
 
-        cr.cancelIncident(3);
+        // cr.dispatch();
+        // cr.tick();
+        // System.out.println(cr.getStatus());
 
-        System.out.println(cr.getStatus());
+        // cr.cancelIncident(3);
 
-        cr.reportIncident(IncidentType.FIRE, 4, 2, 2);
-        cr.tick();
-        System.out.println(cr.getStatus());
+        // System.out.println(cr.getStatus());
 
-        cr.escalateIncident(4, 5);
-        System.out.println(cr.getStatus());
+        // cr.reportIncident(IncidentType.FIRE, 4, 2, 2);
+        // cr.tick();
+        // System.out.println(cr.getStatus());
 
-        cr.decommissionUnit(2);
+        // cr.escalateIncident(4, 5);
+        // System.out.println(cr.getStatus());
 
-        cr.removeStation(1);
+        // cr.decommissionUnit(2);
 
-        System.out.println(cr.getStatus());
+        // cr.removeStation(1);
 
-        cr.addStation("B", 4, 4);
+        // System.out.println(cr.getStatus());
 
-        System.out.println(cr.getStatus());
+        // cr.addStation("A", 4, 4);
 
-        cr.addUnit(2, UnitType.AMBULANCE);
+        // cr.addUnit(1, UnitType.AMBULANCE);
+        // cr.addUnit(1, UnitType.FIRE_ENGINE);
 
-        System.out.println(cr.getStatus());
+        // cr.addObstacle(1,1);
 
-        cr.reportIncident(IncidentType.MEDICAL, 2, 0, 1);
+        // cr.reportIncident(IncidentType.MEDICAL, 2, 0, 1);
 
-        cr.dispatch();
+        // System.out.println(cr.getStatus());
 
-        cr.tick();
-        System.out.println(cr.getStatus());
+        // cr.dispatch();
 
-        cr.tick();
-        System.out.println(cr.getStatus());
+        // cr.tick();
+        // System.out.println(cr.getStatus());
 
-        cr.tick();
-        System.out.println(cr.getStatus());
+        // cr.tick();
+        // System.out.println(cr.getStatus());
 
-        cr.tick();
-        System.out.println(cr.getStatus());
+        // cr.tick();
+        // System.out.println(cr.getStatus());
 
-        cr.tick();
-        System.out.println(cr.getStatus());
+        // cr.tick();
+        // System.out.println(cr.getStatus());
 
-        cr.tick();
-        System.out.println(cr.getStatus());
+        // cr.tick();
+        // System.out.println(cr.getStatus());
 
-        cr.tick();
-        System.out.println(cr.getStatus());
+        // cr.tick();
+        // System.out.println(cr.getStatus());
 
-        cr.tick();
-        System.out.println(cr.getStatus()); //work = 1 at incident
+        // cr.tick();
+        // System.out.println(cr.getStatus());
+
+        // cr.tick();
+        // System.out.println(cr.getStatus()); //work = 1 at incident
+
+        // cr.tick();
+        // System.out.println(cr.getStatus()); //work will reach 2, incident will resolve
+
+        // cr.removeStation(3);
+
+        
         
 
 
